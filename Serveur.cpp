e31a2bb9e176e31127f1e0c62097c1e2a1032fca
Ludovic <cros_ludovic@hotmail.fr>
2017-10-27 18:18:33 +0200

    Modification serveur

--- Serveur.cpp
+++ Serveur.cpp
@@ -6,13 +6,38 @@
  */
 
 #include "Serveur.h"
+#include <iostream>
+#include <vector>
+#include <boost/regex.hpp>
+
+using namespace std;
 
 Serveur::Serveur() {
 	// TODO Auto-generated constructor stub
-
+	this->host = "";
+	this->port = 10000;
 }
 
 Serveur::~Serveur() {
 	// TODO Auto-generated destructor stub
+}
+
+void Serveur::enterInformation() {
+	bool continu = true;
+	boost::regex ipTest ("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+	while(continu) {
+		cout << "enter a valide ip" << endl;
+		string ip;
+		cin >> ip;
+		if(boost::regex_match(ip, ipTest)) {
+			continu = false;
+		}
+	}
+}
+
+int main(void) {
+	Serveur serv;
+	serv.enterInformation();
+	return 0;
 }
 
--- Serveur.h
+++ Serveur.h
@@ -8,10 +8,26 @@
 #ifndef SERVEUR_H_
 #define SERVEUR_H_
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Serveur {
 public:
 	Serveur();
+	void enterInformation();
+	void launch();
+	int connexion(string user, string pass);
+	void receiveMessage();
+	void sendMessages(string message);
 	virtual ~Serveur();
+
+private:
+	vector<string> messages;
+	string host;
+	int port;
+
 };
 
 #endif /* SERVEUR_H_ */
